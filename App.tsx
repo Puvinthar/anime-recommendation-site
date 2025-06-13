ceccee1c3a3dbe39cb4b8505c949edb29ff3f98f
puvinthar s <157071486+Puvinthar@users.noreply.github.com>
2025-06-13 11:10:53 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { Layout } from './components/Layout';
+import { HomePage } from './pages/HomePage';
+import { GenresPage } from './pages/GenresPage';
+import { WatchlistPage } from './pages/WatchlistPage';
+import { AboutPage } from './pages/AboutPage';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/genres" element={<GenresPage />} />
+            <Route path="/watchlist" element={<WatchlistPage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
